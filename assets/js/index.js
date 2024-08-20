7d9105245c4428c8b1422883eea3a836a687275c
Jamshid <jamshid_tojiboyev@mail.ru>
2024-08-20 23:38:09 +0500

    added

--- assets/js/index.js
+++ assets/js/index.js
@@ -49,3 +49,17 @@ const wrapper = new Swiper('.swiper-wr', {
       }
     })
   })
+
+const order_btn = document.querySelectorAll('.order-btn'),
+      body = document.querySelector('body'),
+      popUp_order = document.querySelector('.popUp-order');
+
+order_btn.forEach(btn => {
+  btn.addEventListener('click', () => {
+    popUp_order.classList.remove('d-none');
+    body.classList.add('bodyStopScroll')
+  })
+})
+
+  
+$(".phone").inputmask({"mask": "+7(999) 999-99-99"});
