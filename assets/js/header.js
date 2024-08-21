4fa2eada0d9f405377462fed3249ee18c4c5fdf6
Jamshid <jamshid_tojiboyev@mail.ru>
2024-08-21 12:38:09 +0500

    added

--- assets/js/header.js
+++ assets/js/header.js
@@ -4,4 +4,20 @@ const menu_btn = document.querySelector('.menu-btn'),
 menu_btn.addEventListener('click', () => {
     menu.classList.toggle('showMenu');
     menu_btn.classList.toggle('clickedMenu')
+});
+
+
+
+const order_btn = document.querySelectorAll('.order-btn'),
+      body = document.querySelector('body'),
+      popUp_order = document.querySelector('.popUp-order');
+
+order_btn.forEach(btn => {
+  btn.addEventListener('click', () => {
+    popUp_order.classList.remove('d-none');
+    body.classList.add('bodyStopScroll')
+  })
 })
+
+  
+$(".phone").inputmask({"mask": "+7(999) 999-99-99"});
